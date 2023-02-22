26ce1d6cc3957cccac2c7d97b53a991061becf61
Profesor Vespertino <ProfVespertino@PROFESOR06.informatica.comercio>
2023-02-22 17:45:50 +0100

    Angular web

--- Tema_12/WEB/src/app/app-routing.module.ts
+++ Tema_12/WEB/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AlumnosNoCreativosComponent } from './alumnos-no-creativos/alumnos-no-creativos.component';
+import { ContactPageComponent } from './pages/contact.page/contact.page.component';
+import { FormularioPageComponent } from './pages/formulario.page/formulario.page.component';
+import { HomePageComponent } from './pages/home.page/home.page.component';
 
 const routes: Routes = [
-  { path: '', component: AlumnosNoCreativosComponent }
+  { path: '', component: HomePageComponent },
+  { path: 'contact', component: ContactPageComponent },
+  { path: 'form', component: FormularioPageComponent }
 ];
 
 @NgModule({
--- Tema_12/WEB/src/app/app.module.ts
+++ Tema_12/WEB/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AlumnosNoCreativosComponent } from './alumnos-no-creativos/alumnos-no-creativos.component';
+import { AlumnosNoCreativosComponent } from './components/alumnos-no-creativos/alumnos-no-creativos.component';
+import { HomePageComponent } from './pages/home.page/home.page.component';
+import { ContactPageComponent } from './pages/contact.page/contact.page.component';
+import { FormsModule } from '@angular/forms';
+import { FormularioPageComponent } from './pages/formulario.page/formulario.page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AlumnosNoCreativosComponent
+    AlumnosNoCreativosComponent,
+    HomePageComponent,
+    ContactPageComponent,
+    FormularioPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
