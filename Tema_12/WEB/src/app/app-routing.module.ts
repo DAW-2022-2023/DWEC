5ba3f68c48c71e581549e7cef5bfec1c1e307f4d
Profesor Vespertino <ProfVespertino@PROFESOR06.informatica.comercio>
2023-02-22 16:24:44 +0100

    Angular web

--- Tema_12/WEB/src/app/app-routing.module.ts
+++ Tema_12/WEB/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlumnosNoCreativosComponent } from './alumnos-no-creativos/alumnos-no-creativos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AlumnosNoCreativosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
